2f20670cdd3e4342cc2655e13f83f0b73c2866aa
Rohan Lekhwani <rohanlekhwani@ymail.com>
2022-12-11 01:58:36 -0500

    fix: predict api endpoint

--- server.py
+++ server.py
@@ -12,14 +12,32 @@ app = flask.Flask(__name__)
 
 # load the model, and pass in the custom metric function
 model = load_model('model_ic.h5', custom_objects={'KerasLayer': hub.KerasLayer})
+
+# list of all labels
 labels = ['answer_question_external_fact', 'answer_question_recipe_steps',
        'ask_question_ingredients_tools', 'ask_question_recipe_steps',
        'ask_student_question', 'chitchat', 'misc', 'request_next_step',
        'return_list_ingredients_tools', 'return_next_step', 'stop']
 
+# define a predict function as an endpoint 
+@app.route("/predict", methods=["GET","POST"])
+def predict():
+    data = {}
+
+    params = flask.request.json
+
+    predictions = tf.nn.softmax(model.predict([params['text']]))
+    index = np.argmax(predictions)
+
+    data["intent"] = labels[index]
+    data["score"] = str(np.amax(predictions))
+
+    # return a response in json format 
+    return flask.jsonify(data)    
+
 @app.route('/status', methods=['GET'])
 def get_status():
-    return jsonify({
+    return flask.jsonify({
         'status' : 'alive'
     })
 
